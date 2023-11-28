38a684b469a6c8121f905cafc0586fd4a615c0aa
sharon200102 <sharon200102@gmail.com>
2023-11-28 08:54:39 +0000

    new version due to pr #1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.5' 
+VERSION = '0.1.6' 
 DESCRIPTION = 'A Package that includes utility functions'
 LONG_DESCRIPTION = 'utils package that includes multiple utility functions that are used in different projects'
 
