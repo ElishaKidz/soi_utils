a9e0e94474475c1997c709b7b0623997e171b3e5
sharon200102 <sharon200102@gmail.com>
2023-11-28 09:38:09 +0000

    implmented the Updatable and Resetable interfaces

---
+++ SoiUtils/interfaces.py
@@ -0,0 +1,26 @@
+import abc
+
+class Resetable(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, __subclass: type) -> bool:
+        return hasattr(__subclass,'reset') and callable(__subclass.reset) or NotImplemented
+    
+    @abc.abstractmethod
+    def reset(self, *args,**kwargs):
+        """reset the object"""
+        raise NotImplementedError
+
+ 
+class Updatable(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, __subclass: type) -> bool:
+        return hasattr(__subclass,'update') and callable(__subclass.update) or NotImplemented
+    
+
+    @abc.abstractmethod
+    def update(*args,**kwargs):
+        raise NotImplementedError
+        
+    
+
+    
