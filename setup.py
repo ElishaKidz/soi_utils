99ae911fa72776d171efcfc4c7dbff84f9db1f80
sharon komissarov <sharon200102>
2024-01-08 08:11:51 +0000

    Added the ronen api classes - version 2.0.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.9' 
+VERSION = '0.2.0' 
 DESCRIPTION = 'A Package that includes utility functions'
 LONG_DESCRIPTION = 'utils package that includes multiple utility functions that are used in different projects'
 
